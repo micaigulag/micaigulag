a20acadb8805a06cc9b69e3bb9d130bda748a544
micaigulag <167858662+micaigulag@users.noreply.github.com>
2024-09-30 22:25:48 +0200

    Create defines.php

---
+++ defines.php
@@ -0,0 +1,5 @@
+<?php
+  define('INSTAGRAM_APP_ID','960940072716713');
+  define('INSTAGRAM_APP_SECRET','f514e78954e8ead6cdbd2114329efebd');
+  define('INSTAGRAM_APP_REDIRECT_URI','https://github.com/micaigulag/micaigulag/new/main');
+?>
